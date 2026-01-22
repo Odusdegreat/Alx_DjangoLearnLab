477e57ee08ff78fd1eb5a972780dad7a442a6804
odusdegreat <tobiodubote@gmail.com>
2026-01-22 11:45:20 +0100

    fix: fixed some errors

--- django-models/django_models/urls.py
+++ django-models/django_models/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # Import include to include app urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # Django Admin
+    path('', include('relationship_app.urls')),  # Include the URLs from relationship_app
 ]
