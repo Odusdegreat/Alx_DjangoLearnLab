39ec27c25726a58313b9e1dc8ebfce22b0b70c6c
odusdegreat <tobiodubote@gmail.com>
2026-01-24 14:56:58 +0100

    fix

---
+++ django-models/LibraryProject/relationship_app/templates/relationship_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('register/', views.register, name='register'),  # Register page
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # Login page
+    path('logout/', LogoutView.as_view(), name='logout'),  # Logout functionality
+]
