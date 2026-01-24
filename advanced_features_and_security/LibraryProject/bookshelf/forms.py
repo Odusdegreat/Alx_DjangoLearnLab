8c89ca73549808c098e2d8de4b6fba2292562935
odusdegreat <tobiodubote@gmail.com>
2026-01-24 17:49:40 +0100

    fix

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,13 +1,15 @@
 from django import forms
-from .models import Book
+from .models import Book  # Assuming you have a Book model
 
-# Form to create or edit a book
-class BookForm(forms.ModelForm):
+# Example Form for the Book model (creating or editing a book)
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author', 'published_date', 'isbn']
+        fields = ['title', 'author', 'published_date', 'isbn']  # List of fields you want to include
 
-# Form to search books by author
-class BookSearchForm(forms.Form):
-    author = forms.CharField(max_length=100, required=False)
-    title = forms.CharField(max_length=100, required=False)
+    # You can also add custom validations or field-specific logic here
+    def clean_isbn(self):
+        isbn = self.cleaned_data.get('isbn')
+        if len(isbn) != 13:
+            raise forms.ValidationError("ISBN must be 13 characters long.")
+        return isbn
