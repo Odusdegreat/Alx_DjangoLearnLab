6d5267646ac69fe4f3109e0a7ec488d4ea4bac24
odusdegreat <tobiodubote@gmail.com>
2026-01-24 15:27:51 +0100

    fix

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,20 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book, Library
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Function-based view to list all books
-def list_books(request):
-    """
-    Function-based view that lists all books in the database.
-    """
-    books = Book.objects.all()  # Fetch all books from the database
-    return render(request, 'relationship_app/list_books.html', {'books': books})  # Render the template with books data
-
-
-class LibraryDetailView(DetailView):
-    """
-    Class-based view using DetailView to display library details.
-    """
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+urlpatterns = [
+    path('register/', views.register, name='register'),  # Register page
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # Login page
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logged_out.html'), name='logout'),  # Logout page
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.contrib import messages
 
 # View to register a new user
@@ -8,10 +8,11 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save()  # Save the new user
             login(request, user)  # Automatically log the user in after registration
             messages.success(request, f'Welcome {user.username}!')
-            return redirect('home')  # Redirect to the home page or any other page
+            next_url = request.GET.get('next', 'home')  # Redirect to the 'next' page if provided, otherwise default to 'home'
+            return redirect(next_url)
     else:
         form = UserCreationForm()
 
