eef6417e692de2bf64944da70a80061802565da6
odusdegreat <tobiodubote@gmail.com>
2026-01-25 17:25:13 +0100

    fix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -128,3 +128,14 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/signup.html", {"form": form})
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("list_books")
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})  # <- changed
