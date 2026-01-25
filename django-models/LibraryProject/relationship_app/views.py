212d3b1b52fc228151f6acf0cbf42113aa67f562
odusdegreat <tobiodubote@gmail.com>
2026-01-25 16:42:26 +0100

    omo

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def list_books(request):
+    # Fetch all books
+    books = Book.objects.all()  # <-- MUST have this exact line
+
+    # Render template
+    return render(
+        request,
+        "relationship_app/list_books.html",  # <-- MUST have this exact string
+        {"books": books}
+    )
