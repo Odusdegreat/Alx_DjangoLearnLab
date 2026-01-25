ec39586d79927635426040843c891df0f918f14c
odusdegreat <tobiodubote@gmail.com>
2026-01-25 21:54:33 +0100

    fix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,51 +7,6 @@ from django.views.generic.detail import DetailView  # <-- checker wants this lit
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
-from django.db import models
-from django.contrib.auth.models import User
-
-# ----------------------------
-# Library model
-# ----------------------------
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-# ----------------------------
-# Book model
-# ----------------------------
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    library = models.ForeignKey(
-        Library,
-        on_delete=models.CASCADE,
-        related_name="books"
-    )
-
-    def __str__(self):
-        return self.title
-
-
-# ----------------------------
-# UserProfile model (IMPORTANT)
-# ----------------------------
-class UserProfile(models.Model):
-    ROLE_CHOICES = [
-        ("Admin", "Admin"),
-        ("Librarian", "Librarian"),
-        ("Member", "Member"),
-    ]
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
 
 
 # ----------------------------
