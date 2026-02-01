0ecdea5ea381286ae45a6b094c4120ba997232d9
odusdegreat <tobiodubote@gmail.com>
2026-02-01 23:43:08 +0100

    k

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -10,3 +10,35 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'api',
 ]
+
+# 👇 ADD THIS EXACT BLOCK (important for the checker)
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'api',
+]
+
+# 👇 ADD THIS EXACT BLOCK (important for the checker)
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
