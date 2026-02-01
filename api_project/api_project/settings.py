a15940c66cbf4995fbd3c8b8ae2f923ff31d4ed0
odusdegreat <tobiodubote@gmail.com>
2026-02-01 22:40:38 +0100

    fix

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -7,6 +7,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'rest_framework.authtoken',  # ✅ add this
+    'rest_framework.authtoken',
     'api',
 ]
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -5,9 +5,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Token auth endpoint (username + password -> token)
+    # Token authentication endpoint
     path('api/token/', obtain_auth_token, name='api-token'),
 
-    # Your API app routes
     path('api/', include('api.urls')),
 ]
