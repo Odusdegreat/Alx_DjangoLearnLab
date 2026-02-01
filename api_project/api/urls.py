5ee3de5d0333b919976a6c43518e7ae496353042
odusdegreat <tobiodubote@gmail.com>
2026-02-01 22:32:27 +0100

    fix

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -6,9 +6,6 @@ router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    # Route for the BookList view (ListAPIView)
     path('books/', BookList.as_view(), name='book-list'),
-
-    # Include the router URLs for BookViewSet (all CRUD operations)
     path('', include(router.urls)),
 ]
